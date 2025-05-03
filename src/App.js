973721acb8f51970a0941225f1869e68b40cc931
Петров Игорь <igvapetrov@inno.tech>
2025-05-03 23:00:53 +0300

    Попробуем вот так

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import { useState } from 'react';
 import './App.css';
+import TodoList from './Components/ToDoList';
 
 function App() {
+  const [todos, setTodos] = useState([])
+  const [taskName, setTask] = useState("")
+
+  function addTask(e) {
+    e.preventDefault();
+    setTodos([...todos,taskName]);
+    setTask('');
+  }
+  function deleteTask(index) {
+    const newTodos = [...todos];
+    newTodos.splice(index,1);
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Список дел</h1>
       </header>
+      <div>
+        <form className="list-form">
+          <ul className="dotted-list">
+            {todos.map((item,index) => (
+              <li key={index}>
+                <span className="number">{index+1}</span>
+                <span className="text">{item}</span>
+                <span className="dots"></span>
+                <span><button onClick={() => deleteTask(index)}>Удалить</button></span>
+              </li>
+            ))}
+          </ul>
+        </form>
+        <form className="reg-form">
+          <input type="text" value={taskName} onChange={(e) => setTask(e.target.value)} />
+          <button className="add-button" onClick={addTask}>Добавить задачу</button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/Components/ToDoList.jsx
@@ -0,0 +1,11 @@
+function TodoList (props) {
+
+    return (
+        <div>
+
+        </div>
+    );
+  }
+  
+  export default TodoList;
+  
