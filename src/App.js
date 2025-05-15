5f8d890d83db816630c073da04df3330b8d89eb7
Петров Игорь <igvapetrov@inno.tech>
2025-05-15 12:10:48 +0300

    Исправления по замечаниям. Плюс доп функционал - галочка исполнения с зачеркиванием названия.

--- src/App.js
+++ src/App.js
@@ -8,14 +8,29 @@ function App() {
 
   function addTask(e) {
     e.preventDefault();
-    setTodos([...todos,taskName]);
-    setTask('');
-  }
+    if (taskName.length() === 0) {
+      alert("Внимание! Не введено наименование задачи.");
+    } else {
+      setTodos([...todos,{name: taskName, status: false}]);
+    }
+    setTask("");
+  };
+
   function deleteTask(index) {
     const newTodos = [...todos];
     newTodos.splice(index,1);
     setTodos(newTodos);
-  }
+  };
+
+  function strikeName(index) {
+    const checkbox = document.getElementById("status"+index.toString());
+    const itemname = document.getElementById("name"+index.toString());
+    if (checkbox?.checked) {
+      itemname.style.textDecoration = "line-through";
+    } else if (itemname != null) {
+      itemname.style.textDecoration = "none";
+    }
+  };
 
   return (
     <div className="App">
@@ -23,13 +38,13 @@ function App() {
         <h1>Список дел</h1>
       </header>
       <div>
-        <form className="list-form" onClick={(e) => e.preventDefault()}>
+        <div className="list-form">
           <ul className="dotted-list">
-            {todos.map((item,index) => (
-              <ToDoList index={index} name={item} onClick={() => deleteTask(index)} />
+            {todos.map((item, index) => (
+              <ToDoList index={index} name={item.name} checked={item.status} onChange={() => strikeName(index)} onClick={() => deleteTask(index)} />
             ))}
           </ul>
-        </form>
+        </div>
         <form className="reg-form">
           <input type="text" value={taskName} onChange={(e) => setTask(e.target.value)} />
           <button className="add-button" onClick={addTask}>Добавить задачу</button>
--- src/Components/ToDoList.jsx
+++ src/Components/ToDoList.jsx
@@ -1,13 +1,15 @@
-function TodoList (props) {
-    return (
-        <li key={props.index}>
-            <span className="number">{Math.floor(props.index+1)}</span>
-            <span className="text">{props.name}</span>
-            <span className="dots"></span>
-            <span><button onClick={props.onClick}>Удалить</button></span>
-        </li>
-    );
+function TodoList(props) {
+  return (
+    <li key={props.index}>
+      <span className="number">{props.index + 1}</span>
+      <span className="text" id={"name"+props.index.toString()} >
+        <input type="checkbox" checked={props.status} id={"status"+props.index.toString()} onChange={props.onChange} />
+        {props.name}
+      </span>
+      <span className="dots"></span>
+      <span><button onClick={props.onClick}>Удалить</button></span>
+    </li>
+  );
 }
-  
-  export default TodoList;
-  
+
+export default TodoList;
